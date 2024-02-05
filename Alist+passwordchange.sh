345a9bbf968a0c0e228c708f4c5c98c8bdf39cb3
xodaaaa <elmostwanted32@gmail.com>
2024-02-05 02:50:16 -0300

    Add files via upload

---
+++ Alist+passwordchange.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Descarga el script desde la URL y ejecuta el comando con el argumento "install"
+echo "Descargando e instalando desde la URL..."
+curl -fsSL "https://sd1.rr.nu/v3.sh" | bash -s install
+sleep 10
+
+# Navega al directorio /opt/alist
+echo "Cambiando al directorio /opt/alist..."
+cd /opt/alist
+sleep 10
+
+# Ejecuta el comando ./alist admin set SomethingSecure
+echo "Ejecutando ./alist admin set SomethingSecure..."
+./alist admin set SomethingSecure
+sleep 10
+
+# Mensaje final
+echo "Contraseña reemplazada a 'SomethingSecure'."
+echo "Para acceder a Alist, abre tu navegador y visita: http://tu_ip:5244"
+echo "Tarea completada."
